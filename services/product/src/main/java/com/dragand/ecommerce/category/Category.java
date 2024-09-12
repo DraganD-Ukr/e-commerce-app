66a7f9049d7836980e75dbc98947cbf3709015d0
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-09-12 15:35:17 +0100

    Added Product and Category model classes

---
+++ services/product/src/main/java/com/dragand/ecommerce/category/Category.java
@@ -0,0 +1,25 @@
+package com.dragand.ecommerce.category;
+
+import com.dragand.ecommerce.product.model.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+
+}
