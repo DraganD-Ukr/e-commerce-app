9f67dfc09e1030e4e3246576275c1dc2b857c325
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-09-21 19:23:24 +0100

    Initial API gateway commit

---
+++ services/gateway/src/main/java/com/dragand/gatewa/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dragand.gatewa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
